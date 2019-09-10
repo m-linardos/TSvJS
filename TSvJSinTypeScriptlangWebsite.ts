cd07b2e2ea6e73ec8cc8a0c7aec78e3937a9ec92
Marianne Linardos <mlinardos.edu@gmail.com>
2019-09-10 16:41:00 -0400

    added typescript file

---
+++ TSvJSinTypeScriptlangWebsite.ts
@@ -0,0 +1,13 @@
+Use link to compare TypeScript vs. JavaScript https://www.typescriptlang.org/play/index.html#code/MYewdgziA2CmB00QHMAUAiAEraT0EoBuAKAEM4AnAFwwAtYBPAkuKgAlIC42wBXAWwBGsCmwC8bAOwkAbqVGDxUkgDNeYYFQCW4DgBM9qAA4BGbnyEiANGyMAmTheEV8bcwOdsA3sTZ+2FLBUvBRgtiZsANS2dsTEAL7EoJAwCEhopAaopFaC+ERAA
+
+TypeScript V JavaScript
+
+console.log("Hello");		
+alert("hey");
+let a: number = 7;
+var b = 7;
+function add(p1: number, p2:number) : number {
+    return p1 + p2
+
+}
+console.log(add(a,b));
